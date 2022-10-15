7e7534f8cc5a6bc71bd6c07ac07e7ac8daac455e
Nesim <nessim.jemal@gmail.com>
2022-10-15 15:50:12 -0700

    testing

---
+++ server/server.py
@@ -0,0 +1,30 @@
+import socket
+import os
+
+HOST = '192.168.1.107'  # Standard loopback interface address (localhost)
+PORT = 65431     # Port to listen on (non-privileged ports are > 1023)
+
+dataFromClient = ""
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            #conn.sendall(b"hi!!?")
+            data = conn.recv(1024)
+            print(data)
+            if not data:
+                break
+            dataFromClient = data.decode('utf-8')
+            #print('sending data recieved back...')
+            conn.sendall(data)
+            break
+
+fileName = 'tempfile.py'
+with open(fileName,'w+') as f:
+    f.write(dataFromClient)
+
+os.system(f"python3 {fileName}")
