0a76795271f58546ca64a1e0c085c47ca6a173dc
Nesim <nessim.jemal@gmail.com>
2022-10-15 13:57:03 -0700

    testing

--- server/demo_dir/video_dir/app.py
+++ server/demo_dir/video_dir/app.py
@@ -8,46 +8,118 @@ from flask import Flask, render_template, Response, send_from_directory
 
 from camera_opencv import Camera
 
+# app = Flask(__name__)
+# CORS(app, supports_credentials=True)
+# camera = Camera()
+#
+#
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()
+#
+#         yield (b'--frame\r\n'
+#
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#
+#
+# @app.route('/')
+#
+#
+# def video_feed():
+#     return Response(gen(Camera()),
+#
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+#
+#
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', threaded=True)
+#
+#
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+# @app.route('/api/img/<path:filename>')
+# def sendimg(filename):
+#     return send_from_directory(dir_path+'/dist/img', filename)
+#
+# @app.route('/js/<path:filename>')
+# def sendjs(filename):
+#     return send_from_directory(dir_path+'/dist/js', filename)
+#
+# @app.route('/css/<path:filename>')
+# def sendcss(filename):
+#     return send_from_directory(dir_path+'/dist/css', filename)
+#
+# @app.route('/api/img/icon/<path:filename>')
+# def sendicon(filename):
+#     return send_from_directory(dir_path+'/dist/img/icon', filename)
+
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
+camera = Camera()
 
 
 def gen(camera):
+    """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
-
         yield (b'--frame\r\n'
-
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/')
-
-
+@app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
-
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
+
 @app.route('/api/img/<path:filename>')
 def sendimg(filename):
-    return send_from_directory(dir_path+'/dist/img', filename)
+    return send_from_directory(dir_path + '/dist/img', filename)
+
 
 @app.route('/js/<path:filename>')
 def sendjs(filename):
-    return send_from_directory(dir_path+'/dist/js', filename)
+    return send_from_directory(dir_path + '/dist/js', filename)
+
 
 @app.route('/css/<path:filename>')
 def sendcss(filename):
-    return send_from_directory(dir_path+'/dist/css', filename)
+    return send_from_directory(dir_path + '/dist/css', filename)
+
 
 @app.route('/api/img/icon/<path:filename>')
 def sendicon(filename):
-    return send_from_directory(dir_path+'/dist/img/icon', filename)
+    return send_from_directory(dir_path + '/dist/img/icon', filename)
 
+
+@app.route('/fonts/<path:filename>')
+def sendfonts(filename):
+    return send_from_directory(dir_path + '/dist/fonts', filename)
+
+
+@app.route('/<path:filename>')
+def sendgen(filename):
+    return send_from_directory(dir_path + '/dist', filename)
+
+
+@app.route('/')
+def index():
+    return send_from_directory(dir_path + '/dist', 'index.html')
+
+
+class webapp:
+    def __init__(self):
+        self.camera = camera
+
+    def modeselect(self, modeInput):
+        Camera.modeSelect = modeInput
+
+    def colorFindSet(self, H, S, V):
+        camera.colorFindSet(H, S, V)
+
+    def thread(self):
+        app.run(host='0.0.0.0', threaded=True)  # Thread starts
